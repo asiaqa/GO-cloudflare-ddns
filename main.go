ad2cd0251582eadbf20248cdccd392966cdb0a79
MP <112455607+asiaqa@users.noreply.github.com>
2023-09-09 15:55:23 +0800

    Update main.go
    added ability of ipv4 update

--- main.go
+++ main.go
@@ -5,27 +5,41 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 )
 
-func getIP() (string, error) {
-	resp, err := http.Get("https://api6.ipify.org")
+func getIPv4() (string, error) {
+	resp, err := http.Get("https://api.ipify.org/")
 	if err != nil {
-		return "", fmt.Errorf("Unable to get IP address: %v", err)
+		return "", fmt.Errorf("Unable to get IPv4 address: %v", err)
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return "", fmt.Errorf("Unable to read response body: %v", err)
 	}
 	return string(body), nil
 }
+
+func getIPv6() (string, error) {
+	resp, err := http.Get("https://api6.ipify.org")
+	if err != nil {
+		return "", fmt.Errorf("Unable to get IPv6 address: %v", err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("Unable to read response body: %v", err)
+	}
+	return string(body), nil
+}
+
 func writeToLog(result string) {
-	f, err := os.OpenFile("log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	f, err := os.OpenFile("ddns-log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error opening log file: %v\n", err)
 		return
@@ -38,150 +52,180 @@ func writeToLog(result string) {
 		return
 	}
 }
-func main() {
-	api_key := flag.String("k", "", "Cloudflare API key")
-	ddns_record_name := flag.String("d", "", "DDNS record name")
-	flag.Parse()
-	ipv6_address, err := getIP()
+
+func handleError(errMsg string) {
+	fmt.Fprintln(os.Stderr, errMsg)
+	writeToLog(errMsg)
+	os.Exit(1)
+}
+
+func getZoneName(ddnsRecordName string) string {
+	splitDomain := strings.Split(ddnsRecordName, ".")
+	return fmt.Sprintf("%s.%s", splitDomain[1], splitDomain[2])
+}
+
+func getZoneID(apiKey, zoneName string) string {
+	url := fmt.Sprintf("https://api.cloudflare.com/client/v4/zones?name=%s", zoneName)
+	respBody := makeRequest("GET", url, apiKey, nil)
+	var resultData map[string]interface{}
+	json.Unmarshal([]byte(respBody), &resultData)
+	zoneID := resultData["result"].([]interface{})[0].(map[string]interface{})["id"].(string)
+	if zoneID == "" {
+		handleError(fmt.Sprintf("Unable to get zone ID for domain %s", zoneName))
+	}
+	return zoneID
+}
+
+func getRecordID(apiKey, zoneID, ddnsRecordName string) string {
+	url := fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records?type=AAAA&name=%s", zoneID, ddnsRecordName)
+	respBody := makeRequest("GET", url, apiKey, nil)
+	var resultData map[string]interface{}
+	json.Unmarshal([]byte(respBody), &resultData)
+	resultArray, ok := resultData["result"].([]interface{})
+	if ok && len(resultArray) != 0 {
+		recordID := resultArray[0].(map[string]interface{})["id"].(string)
+		if recordID == "" {
+			handleError("Unable to get record ID")
+		}
+		return recordID
+	}
+	return ""
+}
+
+func getARecord(apiKey, zoneID, ddnsRecordName string) (string, error) {
+	url := fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records?type=A&name=%s", zoneID, ddnsRecordName)
+	respBody := makeRequest("GET", url, apiKey, nil)
+	var resultData map[string]interface{}
+	json.Unmarshal([]byte(respBody), &resultData)
+	resultArray, ok := resultData["result"].([]interface{})
+	if ok && len(resultArray) != 0 {
+		recordContent := resultArray[0].(map[string]interface{})["content"].(string)
+		if recordContent == "" {
+			return "", fmt.Errorf("Unable to get A record content")
+		}
+		return recordContent, nil
+	}
+	return "", fmt.Errorf("A record not found")
+}
+
+func updateDNSRecord(apiKey, zoneID, recordID, ddnsRecordName, ipv6Address string) {
+	url := fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records/%s", zoneID, recordID)
+	jsonStr := fmt.Sprintf(`{"type":"AAAA","name":"%s","content":"%s","ttl":120}`, ddnsRecordName, ipv6Address)
+	reqBody := []byte(jsonStr)
+	makeRequest("PUT", url, apiKey, reqBody)
+}
+
+func updateDNSRecordIPv4(apiKey, zoneID, recordID, ddnsRecordName, ipv4Address string) {
+	url := fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records/%s", zoneID, recordID)
+	jsonStr := fmt.Sprintf(`{"type":"A","name":"%s","content":"%s","ttl":120}`, ddnsRecordName, ipv4Address)
+	reqBody := []byte(jsonStr)
+	makeRequest("PUT", url, apiKey, reqBody)
+}
+
+func createDNSRecord(apiKey, zoneID, ddnsRecordName, ipv6Address string) {
+	url := fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records", zoneID)
+	jsonStr := fmt.Sprintf(`{"type":"AAAA","name":"%s","content":"%s","ttl":120}`, ddnsRecordName, ipv6Address)
+	reqBody := []byte(jsonStr)
+	makeRequest("POST", url, apiKey, reqBody)
+}
+
+func createDNSRecordIPv4(apiKey, zoneID, ddnsRecordName, ipv4Address string) {
+	url := fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records", zoneID)
+	jsonStr := fmt.Sprintf(`{"type":"A","name":"%s","content":"%s","ttl":120}`, ddnsRecordName, ipv4Address)
+	reqBody := []byte(jsonStr)
+	makeRequest("POST", url, apiKey, reqBody)
+}
+
+func makeRequest(method, url, apiKey string, requestBody []byte) []byte {
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(requestBody))
 	if err != nil {
-		result := fmt.Sprintf("Error getting IP address: %v", err)
-		fmt.Fprintln(os.Stderr, result)
-		writeToLog(result)
-		os.Exit(1)
+		handleError(fmt.Sprintf("Failed to create request: %v", err))
 	}
-	if *api_key == "" || *ddns_record_name == "" {
-		result := "API key or ddns record name not provided"
-		fmt.Fprintln(os.Stderr, result)
-		writeToLog(result)
-		os.Exit(1)
-	}
-	zone_name := fmt.Sprintf("%s.%s", strings.Split(*ddns_record_name, ".")[1], strings.Split(*ddns_record_name, ".")[2])
-	url := fmt.Sprintf("https://api.cloudflare.com/client/v4/zones?name=%s", zone_name)
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		result := fmt.Sprintf("Failed to create request: %v\n", err)
-		fmt.Fprintf(os.Stderr, result)
-		writeToLog(result)
-		os.Exit(1)
-	}
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", *api_key))
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", apiKey))
 	req.Header.Add("Content-Type", "application/json")
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		result := fmt.Sprintf("Failed to make request: %v\n", err)
-		fmt.Fprintf(os.Stderr, result)
-		writeToLog(result)
-		os.Exit(1)
+		handleError(fmt.Sprintf("Failed to make request: %v", err))
+	}
+	defer resp.Body.Close()
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		handleError(fmt.Sprintf("Failed to read response body: %v", err))
 	}
 	if resp.StatusCode != 200 {
-		result := fmt.Sprintf("Unable to get zone ID for domain %s", zone_name)
-		fmt.Fprintln(os.Stderr, result)
-		writeToLog(result)
-		os.Exit(1)
+		handleError(fmt.Sprintf("Request failed with status code %d\nResponse Body: %s", resp.StatusCode, respBody))
 	}
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	var resultData map[string]interface{}
-	json.Unmarshal([]byte(body), &resultData)
-	zone_id := resultData["result"].([]interface{})[0].(map[string]interface{})["id"]
-	zone_idStr, ok := zone_id.(string)
-	if !ok {
-		result := "Unable to get zone ID"
-		fmt.Fprintf(os.Stderr, result)
-		writeToLog(result)
-		os.Exit(1)
-	}
-	url = fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records?type=AAAA&name=%s", zone_idStr, *ddns_record_name)
-	req, err = http.NewRequest("GET", url, nil)
-	if err != nil {
-		result := fmt.Sprintf("Failed to create request: %v\n", err)
-		fmt.Fprintf(os.Stderr, result)
-		writeToLog(result)
-		os.Exit(1)
-	}
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", *api_key))
-	req.Header.Add("Content-Type", "application/json")
-	resp, err = client.Do(req)
-	if err != nil {
-		result := fmt.Sprintf("Failed to make request: %v\n", err)
-		fmt.Fprintf(os.Stderr, result)
-		writeToLog(result)
-		os.Exit(1)
-	}
-	defer resp.Body.Close()
-	body, _ = ioutil.ReadAll(resp.Body)
-	json.Unmarshal([]byte(body), &resultData)
-	resultArray, ok := resultData["result"].([]interface{})
-	if !ok || len(resultArray) == 0 {
-		// No record found, create a new record
-		url = fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records", zone_idStr)
-		jsonStr := fmt.Sprintf(`{"type":"AAAA","name":"%s","content":"%s","ttl":120}`, *ddns_record_name, ipv6_address)
-		reqBody := []byte(jsonStr)
-		req, err = http.NewRequest("POST", url, bytes.NewBuffer(reqBody))
+	return respBody
+}
+
+var lastIPv6Address string
+var lastIPv4Address string
+
+func main() {
+	apiKey := flag.String("k", "", "Cloudflare API key")
+	ddnsRecordName := flag.String("d", "", "DDNS record name")
+	monitorInterval := flag.Duration("m", 60*time.Minute, "Monitoring interval")
+	flag.Parse()
+	lastIPv6Address = ""
+	lastIPv4Address = ""
+
+	for {
+		ipv6Address, err := getIPv6()
 		if err != nil {
-			result := fmt.Sprintf("Failed to create request: %v\n", err)
-			fmt.Fprintf(os.Stderr, result)
-			writeToLog(result)
-			os.Exit(1)
+			handleError(fmt.Sprintf("Error getting IPv6 address: %v", err))
 		}
-		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", *api_key))
-		req.Header.Add("Content-Type", "application/json")
-		resp, err = client.Do(req)
+
+		ipv4Address, err := getIPv4()
 		if err != nil {
-			result := fmt.Sprintf("Failed to make request: %v\n", err)
-			fmt.Fprintf(os.Stderr, result)
-			writeToLog(result)
-			os.Exit(1)
+			handleError(fmt.Sprintf("Error getting IPv4 address: %v", err))
 		}
-		defer resp.Body.Close()
-		if resp.StatusCode != 200 {
-			result := "Failed to create DNS record"
-			fmt.Fprintf(os.Stderr, result)
-			writeToLog(result)
-			os.Exit(1)
+
+		if *apiKey == "" || *ddnsRecordName == "" {
+			handleError("API key or DDNS record name not provided")
 		}
-		result := "DNS record created successfully"
-		fmt.Println(result)
-		writeToLog(result)
-	} else {
-		// Record found, update the existing record with the new IPv6 address
-		record_id := resultArray[0].(map[string]interface{})["id"]
-		record_idStr, ok := record_id.(string)
-		if !ok {
-			result := "Unable to get record ID"
-			fmt.Fprintf(os.Stderr, result)
-			writeToLog(result)
-			os.Exit(1)
+
+		if ipv6Address != lastIPv6Address {
+			zoneName := getZoneName(*ddnsRecordName)
+			zoneID := getZoneID(*apiKey, zoneName)
+			recordID := getRecordID(*apiKey, zoneID, *ddnsRecordName)
+
+			if recordID != "" {
+				updateDNSRecord(*apiKey, zoneID, recordID, *ddnsRecordName, ipv6Address)
+				fmt.Println("IPv6 DNS record updated successfully, ip:", ipv6Address)
+			} else {
+				createDNSRecord(*apiKey, zoneID, *ddnsRecordName, ipv6Address)
+				fmt.Println("IPv6 DNS record created successfully, ip:", ipv6Address)
+			}
+
+			lastIPv6Address = ipv6Address
 		}
-		url = fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records/%s", zone_idStr, record_idStr)
-		jsonStr := fmt.Sprintf(`{"type":"AAAA","name":"%s","content":"%s","ttl":120}`, *ddns_record_name, ipv6_address)
-		reqBody := []byte(jsonStr)
-		req, err = http.NewRequest("PUT", url, bytes.NewBuffer(reqBody))
-		if err != nil {
-			result := fmt.Sprintf("Failed to create request: %v\n", err)
-			fmt.Fprintf(os.Stderr, result)
-			writeToLog(result)
-			os.Exit(1)
+
+		if ipv4Address != lastIPv4Address {
+			zoneName := getZoneName(*ddnsRecordName)
+			zoneID := getZoneID(*apiKey, zoneName)
+			recordID := getRecordID(*apiKey, zoneID, *ddnsRecordName)
+			recordContent, err := getARecord(*apiKey, zoneID, *ddnsRecordName)
+			if err != nil {
+				handleError(fmt.Sprintf("Error retrieving A record: %v", err))
+			}
+			if recordID != "" {
+				if recordContent != ipv4Address {
+					updateDNSRecordIPv4(*apiKey, zoneID, recordID, *ddnsRecordName, ipv4Address)
+					fmt.Println("IPv4 DNS record updated successfully, ip:", ipv4Address)
+				} else {
+					fmt.Println("IPv4 DNS record has not updated, ip:", ipv4Address)
+				}
+
+			} else {
+				createDNSRecordIPv4(*apiKey, zoneID, *ddnsRecordName, ipv4Address)
+				fmt.Println("IPv4 DNS record created successfully, ip:", ipv4Address)
+			}
+
+			lastIPv4Address = ipv4Address
 		}
-		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", *api_key))
-		req.Header.Add("Content-Type", "application/json")
-		resp, err = client.Do(req)
-		if err != nil {
-			result := fmt.Sprintf("Failed to make request: %v\n", err)
-			fmt.Fprintf(os.Stderr, result)
-			writeToLog(result)
-			os.Exit(1)
-		}
-		defer resp.Body.Close()
-		if resp.StatusCode != 200 {
-			result := "Failed to update DNS record"
-			fmt.Fprintf(os.Stderr, result)
-			writeToLog(result)
-			os.Exit(1)
-		}
-		result := "DNS record updated successfully"
-		fmt.Println(result)
-		writeToLog(result)
+		writeToLog("Current: IPV4: " + lastIPv4Address + " Current: IPV6: " + lastIPv6Address)
+		fmt.Println("Check IPv6 and IPv4 again in", *monitorInterval)
+		time.Sleep(*monitorInterval)
 	}
 }
